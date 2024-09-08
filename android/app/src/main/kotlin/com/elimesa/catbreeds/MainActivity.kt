1e89d09e0f16f3993187c29f475c08be0fed20d4
Eliana Mejia <kataeli@gmail.com>
2024-09-07 22:42:04 -0500

    [main] init with the skeleton project

---
+++ android/app/src/main/kotlin/com/elimesa/catbreeds/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.elimesa.catbreeds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:catbreeds/src/app.dart';
+
+void main() {
+  runApp(App());
+}
